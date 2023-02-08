e40fc1ad67e5d807a2e10cfbda36cee9261436a2
Wailodisee <elbelhadjiwail@gmail.com>
2023-02-08 23:58:34 +0100

    herhalingoef

---
+++ SlnLes01HerhalingAanvulling/ConsoleTafels/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTafels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrukTafel (4, 8);
+            DrukTafel (2, 5);
+            VraagPositiefGetal();
+            Console.ReadLine();
+        }
+
+        public static void DrukTafel(int cijfer, int lengte)
+        {
+            Console.WriteLine(lengte + " x " + cijfer + " tafel:");
+
+            for (int i = 1; i <= lengte; i++)
+            {
+                int terugCijfer = cijfer * i;
+                Console.WriteLine(cijfer + " x " + i + " = " + terugCijfer);
+            }
+            Console.WriteLine();
+        }
+
+        public static void VraagPositiefGetal()
+        {
+            Console.Write(" Geef een getal : ");
+            int cijfer = Convert.ToInt32(Console.ReadLine());
+
+            if (cijfer <= 0)
+            {
+                Console.Write(" Het getal moet positief zijn ! ");
+
+                VraagPositiefGetal();
+            }
+            else
+            {
+                Console.Write(" geef de lengte : ");
+
+                int lengte = Convert.ToInt32(Console.ReadLine());
+
+                DrukTafel(cijfer, lengte);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SlnLes01HerhalingAanvulling/WpfTaken/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTaken
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+ 
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+           
+            string text = "";
+            if (rbn1.IsChecked == true)
+            {
+                text = " door: Adam)";
+            }
+            else if (rbn2.IsChecked == true)
+            {
+                text = " door: Bilal)";
+            }
+            else if (rbn3.IsChecked == true)
+            {
+                text = " door: Chelsey)";
+            }
+
+            ListBoxItem item = new ListBoxItem();
+            item.Content = txtTaak.Text + " ( deadline: " + datePicker.SelectedDate.Value.ToString("dd/MM/yyyy ;") + text;
+            lbx1.Items.Add(item);
+        }
+        /*private void ClearListBox()
+        {
+            lbx1.Items.Clear();
+        }*/
+    }
+}
