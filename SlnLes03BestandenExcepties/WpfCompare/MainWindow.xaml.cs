9ea39a2584802adb9c1f831b82a87d3b88449c1c
Wailodisee <elbelhadjiwail@gmail.com>
2023-03-01 22:09:00 +0100

    taak03

--- SlnLes03BestandenExcepties/WpfCompare/MainWindow.xaml.cs
+++ SlnLes03BestandenExcepties/WpfCompare/MainWindow.xaml.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace WpfCompare
 {
@@ -17,80 +15,38 @@ namespace WpfCompare
         {
             InitializeComponent();
 
-            kader1();
-            kader2();
+            PopulateList(lbx1, "kader1");
+            PopulateList(lbx2, "kader2");
         }
-        private void kader1()
+
+        private void PopulateList(ListBox listBox, string Namefolders)
         {
-            string Path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string mijnKader1 = System.IO.Path.Combine(Path, "kader1");
+            string Path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
-            string[] Mijnfiles = Directory.GetFiles(mijnKader1, "Tekstbestanden|*.TXT;*.TEXT");
+            string aantalfolders = System.IO.Path.Combine(Path, Namefolders);
 
-            foreach (string fileName in Mijnfiles)
+            string[] Mijnfiles = Directory.GetFiles(aantalfolders, "*.txt");
+
+            foreach (string NameOfFiles in Mijnfiles)
             {
-                lbx1.Items.Add(System.IO.Path.GetFileName(fileName));
+                listBox.Items.Add(System.IO.Path.GetFileName(NameOfFiles));
             }
-            Console.ReadLine();
         }
-        private void kader2()
-        {
-            string Path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string mijnKader2 = System.IO.Path.Combine(Path, "kader2");
 
-            string[] mijnnFiles = Directory.GetFiles(mijnKader2, "*.txt");
-
-            foreach (string fileName in mijnnFiles)
-            {
-                lbx2.Items.Add(System.IO.Path.GetFileName(fileName));
-            }
-            Console.ReadLine();
-        }
-        private void Kader2(string Path)
+        private void DisplayLines(ListBox listBox, string Path)
         {
-            List<string> mijnLijnen = new List<string>();
+            List<string> Regels = new List<string>();
+
             using (StreamReader reader = new StreamReader(Path))
             {
-                string regels;
-                while ((regels = reader.ReadLine()) != null)
+                string MijnLines;
+
+                while ((MijnLines = reader.ReadLine()) != null)
                 {
-                    mijnLijnen.Add(regels);
+                    Regels.Add(MijnLines);
                 }
             }
-            lbx2.ItemsSource = mijnLijnen;
-
-            Console.ReadLine();
-        }
-        private void Kader1(string filePath)
-        {
-            List<string> aantalRegels = new List<string>();
-            using (StreamReader reader = new StreamReader(filePath))
-            {
-                string REgels;
-                while ((REgels = reader.ReadLine()) != null)
-                {
-                    aantalRegels.Add(REgels);
-                }
-            }
-            lbx1.ItemsSource = aantalRegels;
-            Console.ReadLine();
-        }
- 
-        private void lbxbeneden2_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            string selectedFile = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "kader2", (string)lbx2.SelectedItem);
-
-            kader2(selectedFile);
-
-            Console.ReadLine();
-        }
-        private void lbxbeneden1_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            string selectedFile = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "kader1", (string)lbx1.SelectedItem);
-
-            kader1(selectedFile);
-
-            Console.ReadLine();
+            listBox.ItemsSource = Regels;
         }
     }
 }
