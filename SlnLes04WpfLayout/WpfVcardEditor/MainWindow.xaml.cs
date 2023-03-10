582fdf60c38f117dbc91724ce2ac669f4c53240d
Wailodisee <elbelhadjiwail@gmail.com>
2023-03-10 21:48:01 +0100

    Taak vCard

--- SlnLes04WpfLayout/WpfVcardEditor/MainWindow.xaml.cs
+++ SlnLes04WpfLayout/WpfVcardEditor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,22 +25,24 @@ namespace WpfVcardEditor
         {
             InitializeComponent();
         }
-        /* Window voor afsluiten*/
-        private void submenu_Exit_Click(object sender, RoutedEventArgs e)
+        
+ 
+           
+ 
+        /* Window afsluiten*/
+        private void subItemExit_Click(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult meldingtonen = MessageBox.Show("Ben je zeker dat je de applicatie wil afsluiten?",
+            MessageBoxResult meldingtonen = MessageBox.Show("Ben je zeker dat je de applicatie wil afsluiten?", 
             "Toepassing sluiten", MessageBoxButton.OKCancel);
 
             if (meldingtonen == MessageBoxResult.OK)
             {
-                this.Close();
-            }
-            else if (meldingtonen == MessageBoxResult.Cancel)
-            {
+                Application.Current.MainWindow.Close();
             }
         }
-           /* Window tonen */
-        private void submenu_About_Click(object sender, RoutedEventArgs e)
+        
+        /* Window tonen */
+        private void subItemAbout_Click(object sender, RoutedEventArgs e)
         {
             (new myPopUp()).Show();
         }
