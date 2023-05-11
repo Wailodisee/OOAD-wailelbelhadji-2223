44eed929d80bf7258649bc246d143a76508602f3
Wailodisee <elbelhadjiwail@gmail.com>
2023-05-11 23:46:59 +0200

    eerste Push

---
+++ SlnBuurlenen/MyClassLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    public class Auto
+    {
+        public string model { get; set; }
+
+        public static List<Auto> GetAllAutos()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+
+            //SQL in WPF
+        }
+
+    }
+}
